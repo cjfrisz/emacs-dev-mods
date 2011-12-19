219e879befed5101ed1f03c8e39de878fa72a3f9
Chris Frisz <cjfrisz@chrisfrisz.com>
2011-12-19 14:05:45 -0500

    Correcting block-comment-decorator setting for c-mode.
    Changing the erroneous setting of the block-comment-decorator string
    for C-like modes. Previously it was set on changing to a
    C-mode. However, if multiple buffers with different modes were open,
    this would set the variable for all of them. It has been corrected
    such that the hook is added on "before-change-hook." The associated
    function checks if buffer is in C-mode, and changes the variable
    appropriately. Otherwise, it ensures that the variable is set to an
    otherwise appropriate value.

--- lang/c.el
+++ lang/c.el
@@ -10,4 +10,11 @@
 
 ;; I prefer the '*' character for the block comment decorators in
 ;; languages with C-like, multi-line block comments.
-(add-hook 'c-mode-common-hook '(lambda () (set 'block-comment-decorator "*")))
+(let ((c-comment-decorator "*"))
+  (add-hook 'before-change-hook 
+	    '(lambda () 
+	       (let ((cur-decorator block-comment-decorator))
+		 (if (eq major-mode 'c-mode)
+		     (set 'block-comment-decorator 
+			  c-comment-decorator)
+		   (set 'block-comment-decorator cur-decorator))))))
