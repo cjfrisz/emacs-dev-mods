ed7d2128452499c7b232e484daba25cc2d41175a
Chris Frisz <cjfrisz@chrisfrisz.com>
2011-12-19 14:16:02 -0500

    Finishing rename of lisp/load.el to lisp/lisp-load.el

---
+++ lisp/lisp-load.el
@@ -0,0 +1,19 @@
+;;-- Load files --;;
+;; NOTE: This requires that the "lisp" directory has been added to the
+;; laod-path variable
+
+;; Use helpers.el for the "load-file-list" function
+(load-file "helpers.el")
+
+;; The list of custom elisp files
+(setq lisp-files '("file-doc.el"
+		   "helpers.el" ; Redundant, I know
+		    "update-modify.el"))
+
+;; Perform the actual load
+(load-file-list lisp-files)
+
+;;-- Additional configuration --;;
+
+;; Set the before-save hook to update the modify date
+(add-hook 'before-save-hook 'update-modify)
