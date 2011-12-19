28750f8ab0dcbe01342d502f2d5f4c8f8d36533d
Chris Frisz <cjfrisz@chrisfrisz.com>
2011-12-19 14:18:14 -0500

    Adding 'lang/lang-load.el' to properly perform load on lang files.
    A language-customization loading file that reflects lisp/lisp-load.el
    has been added to simplify the load process in other files (namely
    .emacs).

---
+++ lang/lang-load.el
@@ -0,0 +1,17 @@
+;;-- Load files --;;
+;; NOTE: This requires that the "lang" directory has been added to the
+;; load-path variable
+
+;; This file uses the "load-file-list" function from helpers.el
+;; Note that this requires its directory to be part of the load-path
+;; variable
+(load-file "helpers.el")
+
+;; The list of defined language files
+(setq lang-files '("c.el"
+		   "lisp.el"
+		   "scheme.el"))
+
+
+;; Perform the actual load
+(load-file-list lang-files)
