7ecfd3cd56721bde860b60d1621984a36da8b9d0
Chris Frisz <cjfrisz@chrisfrisz.com>
2011-12-19 14:13:14 -0500

    Adding a set of global helper functions in 'lisp/helpers.el.'
    I've determined that there are several functions that either are
    useful in multiple places or will be in the future. These have been
    collected into 'lisp/helpers.el,' and are loaded globally and used
    across files in the repo.

---
+++ lisp/helpers.el
@@ -0,0 +1,22 @@
+(defun empty? (e)
+  "A simple predicate to test if the argument is the empty list."
+  (eq e ()))
+
+(defun load-file-list (file-list &optional path)
+  "Load a list of files either as fully-qualified paths or files
+in directories included in the load-path. Optionally, a path can
+be provided, assumed to be the path for all files in file-list."
+  (while (not (empty? file-list))
+    (let* ((file (car file-list))
+	   (file-path (concat path 
+			      (convert-standard-filename file))))
+      (progn
+	(load file-path)
+	(setq file-list (cdr file-list))))))
+
+(defun insert-line (&rest line)
+  "Insert a line followed by a newline character and moves the
+point according to the tab command."
+  (progn
+    (apply 'insert (append line '("\n")))
+    (indent-for-tab-command)))
