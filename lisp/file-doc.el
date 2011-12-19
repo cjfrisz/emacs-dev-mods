52e5e947a0b74062381647e726374363ec2b50f9
Chris Frisz <cjfrisz@chrisfrisz.com>
2011-12-19 14:09:41 -0500

    Refactoring insert-file-doc to not make use of previously-defined global variables.
    A previous commit (don't bother me for which one it was) removed
    several block-comment, related global variables. The logic in
    insert-file-doc has been updated to use built-in variables to
    determine proper values for headers, footers, and line-start strings.

--- lisp/file-doc.el
+++ lisp/file-doc.el
@@ -1,50 +1,56 @@
+;; Requires "insert-line" from helpers.el
+(load "helpers.el")
+
 (defun insert-file-doc ()
   "Insert basic file-level documentation"
   (interactive)
-  (let ((decorator-header-num 70))
-    ;; The variable block-comment-start is custom-defined for all
-    ;; languages. However, block-comment-end is only (custom-)defined
-    ;; for languages for which the standard variable
-    ;; comment-multi-line is non-nil
-    (let ((doc-header 
- 	   (concat block-comment-start
- 		   (make-string decorator-header-num 
- 				(string-to-char block-comment-decorator)))))
-      (let ((doc-footer (if (not (eq comment-multi-line nil))
-			    (concat 
-			     (make-string 
-			      decorator-header-num 
-			      (string-to-char block-comment-decorator))
-			     block-comment-end)
-			  doc-header))
-	    (doc-line-start (if (not (eq comment-multi-line nil))
-				(concat block-comment-decorator " ")
-			      (concat block-comment-start " "))))
-	(progn
-	  ;; Move the point to the beginning of the buffer first so we
-	  ;; don't put the documentation in a weird place
-	  (goto-char (point-min))
-	  
-	  ;; Insert the header
-	  (insert doc-header "\n")
-	  ;; Insert the file information 
-	  (insert doc-line-start "File " (buffer-name) "\n")
-	  (indent-for-tab-command)
-	  (insert doc-line-start "Written by " (user-full-name)  "\n")
-	  (indent-for-tab-command)
-	  (insert doc-line-start "\n")
-	  (indent-for-tab-command)
-	  (let ((date (if (boundp 'custom-date-format)
-			  (format-time-string custom-date-format)
-			(current-time-string))))
-	    (insert doc-line-start "Created " date  "\n")
-	    (indent-for-tab-command)
-	    (insert doc-line-start "Last modified " date  "\n")
-	    (indent-for-tab-command))
-	  (insert doc-line-start "\n")
-	  (indent-for-tab-command)
-	  (insert doc-line-start "\n")
-	  (indent-for-tab-command)
-	  ;; Insert the footer
-	  (insert doc-footer)
-	  (indent-for-tab-command))))))
+  (let* ((decorator-seq-len 70)
+	 (decorator-count (/ decorator-seq-len 
+			     (length block-comment-decorator)))
+	 (decorator (make-string 
+		     decorator-seq-len 
+		     (string-to-char block-comment-decorator))))
+    (let* ((comment-start (when comment-multi-line
+			    (substring 
+			     comment-start 
+			     0 
+			     (- (length comment-start) 1))))
+	   (comment-end (if comment-multi-line
+			    (substring comment-end 
+				       1 
+				       (length comment-end))
+			  nil)))
+      (let* ((doc-header (concat comment-start decorator))
+	     (doc-footer (if comment-end
+			     (concat decorator comment-end)
+			   doc-header))
+	     (doc-line-start (if comment-multi-line
+				 (concat block-comment-decorator
+					 " ")
+			       (concat comment-start " "))))
+	(let ((insert-doc-line '(lambda (&rest line)
+				  (apply 'insert-line 
+					 (cons doc-line-start line)))))
+	  (progn
+	    ;; Move the point to the beginning of the buffer first so we
+	    ;; don't put the documentation in a weird place
+	    (goto-char (point-min))
+	    
+	    ;; Insert the header
+	    (insert-line doc-header)
+	    
+	    ;; Insert the file information
+	    (funcall insert-doc-line "File " (buffer-name))
+	    (funcall insert-doc-line "Written by " (user-full-name))
+	    (funcall insert-doc-line)
+	    (let ((date (if (boundp 'custom-date-format)
+			    (format-time-string custom-date-format)
+			  (current-time-string))))
+	      (funcall insert-doc-line "Created " date)
+	      (funcall insert-doc-line "Last modified " date))
+	    (funcall insert-doc-line)
+	    (funcall insert-doc-line)
+	    
+	    ;; Insert the footer
+	    (insert-line doc-footer)))))))
+  
